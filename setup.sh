714f0545823dcf43ce0c8e226e4da6bd80b027cd
r0zbot <r0zbot@users.noreply.github.com>
2019-04-21 18:28:58 -0300

    added missing argument to ln

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ sed -i "s/DIRPATH/$(pwd)/"
 
 mv service.service $sessionname.service
 
-read -e -p "Link container service? [y/n]: " choice
-[[ "$choice" == [Yy]* ]] && sudo ln -s $sessionname.service
-echo 
+read -e -p "Link container service into systemd? [y/n]: " choice
+[[ "$choice" == [Yy]* ]] && sudo ln -s "$pwd"/$sessionname.service /etc/systemd/system/$sessionname.service
+
+echo Done! Dont forget to modify your docker-compose file!
