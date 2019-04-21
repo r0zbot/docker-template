41db63814a504c5e15108c06012daafbd9786f5e
r0zbot <r0zbot@users.noreply.github.com>
2019-04-21 20:01:05 -0300

    fixed linking and added daemon-reload

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ sed -i "s@DIRPATH@$(pwd)@" service.service
 mv service.service $sessionname.service
 
 read -e -p "Link container service into systemd? [y/n]: " choice
-[[ "$choice" == [Yy]* ]] && sudo ln -s "$pwd"/$sessionname.service /etc/systemd/system/$sessionname.service
+[[ "$choice" == [Yy]* ]] && sudo ln -s "$(pwd)/$sessionname.service" /etc/systemd/system/$sessionname.service && sudo systemctl daemon-reload
 
 echo Done! Dont forget to modify your docker-compose file!
